3c3ef67ca80e78b04827be2cf999769afb7fb421
ciccio-415 <100956742+ciccio-415@users.noreply.github.com>
2022-03-10 17:13:36 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -6,5 +6,5 @@ library (raster)
 # settaggio cartella di lavoro all'interno del percorso "C"
 setwd("C:/lab/") # windows
 
-# tramine la funzione brick importiamo la nostra immagine dal paccheto che abbiamo scaricato.
+# tramine la funzione brick importiamo la nostra immagine dal paccheto che abbiamo scaricato. la rinominiamo l2011 per facilitare il lavoro successivo
 l2011 <- brick ("p224r63_2011.grd")
