35c60a022e0e9170df5b974975ec3ee606af5ec8
ciccio-415 <100956742+ciccio-415@users.noreply.github.com>
2022-03-18 17:53:45 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -26,3 +26,64 @@ plot(l2011, col=cl)
 # b2 = verde
 # b3 = rosso
 # b4 = = infrarosso vicino NIR
+# plottiamo la banda del blu, abbiamo vari modi per falro - B1_sre
+plot (l2011$B1_sre)
+#per legare le bande a un immagine satellitare o altre cose usiamo il dollaro per legare insime le cose
+# altro modo per fare lo stesso procedimento, si ripete plot ma invece di dare il nome si prende il primo elemento ovvero la banda del blu
+
+# concetto di elemtno, una lista di dati. in r gli elemtni si racchiudono in doppia parentesi quandra, elementi allintenro delnostro datased dell imagine satellitare.
+plot (l2011[[1]])
+# secondo modo per plottre la prima banda ovvero la banda del blu. i 2 metodi sono ugaali ma ci osno dei pro e i contro ma sono la stessa cosa 
+# banda con nuova legenda
+plot (l2011[[1]])
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot (l2011$B1_sre, col= cl)
+# plot b1 from dark blue to blue to light blue
+clb <- colorRampPalette(c("darkblue", "blue", "light blue")) (100)
+plot (l2011$B1_sre, col= clb)
+# abbiamo riplottato tutto con la scala del blu, rinomincando cl in clb per indicare che si tratta di una scala di colori in blu. 
+# si può esportare nella carella lab un file in pdf utilizzando la funzione pdf e nominando il file.pdf e le virgolette pe uscire da r, per choiudere le cuciture relazionati ai gravici in r si usa una funzione chimaata dev.off
+pdf("banda1.pdf")
+plot (l2011$B1_sre, col= clb)
+# dev.off chiude la finestra, se io apro una finestra vortuale come pdf la choudo con questa funzione
+dev.off()
+# se volessi cambiare cartella di salvataggio cabio il path
+# con la funzione png ottengo la stessa ma con una risoluzoone minore
+png("banda1.pdf")
+plot (l2011$B1_sre, col= clb)
+dev.off()
+# se vogliamo esportare un immagine e non il plot usiamo la funzione writeRaster in cui si esporta tutto il dataset
+# plottiamo più immagini insieme, plottando insieme la banda del blu e del verde ma prima plottiamo la banda del verde (b2) con una nuova scala di colori.
+clg <- colorRampPalette(c("darkgreen", "green", "light green")) (100)
+plot (l2011$B2_sre, col= clg)
+# ora mettiamo insieme le due bande del blu e del verde (b1 e b2)
+# utiliazziamo la funzione par per definire il multiframe
+par (mfrow= c(1,2))
+plot (l2011$B1_sre, col= clb)
+plot (l2011$B2_sre, col= clg)
+# per chiudere la finestra non utilizzando il mouse si utilizza la funzione dev.off()
+# per esportare un pdf del mio multiframe
+# export multiframe plot
+pdf("multiframe.pdf")
+par(mfrow=c(1,2))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+dev.off()
+# esercizio: revert the multiframe ovvero vogliamo fare un plot con il blu sopra e il verde sotto quindi ragionando in termini di righe e colonne nel par quindi in questo caso par(mfrow=c(2,1)).
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+# applichiamo la stessa funzione utilizzando tutte le 4 bande
+
+# let's plot the first four bands
+par(mfrow=c(2,2))
+# blue
+plot(l2011$B1_sre, col=clb) 
+# green
+plot(l2011$B2_sre, col=clg) 
+# red
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(l2011$B3_sre, col=clr)
+# NIR
+clnir <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(l2011$B4_sre, col=clnir)
