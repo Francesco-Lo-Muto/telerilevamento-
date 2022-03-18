884882ae9393e4a7c42f99d826da2b89662188b9
ciccio-415 <100956742+ciccio-415@users.noreply.github.com>
2022-03-18 16:36:23 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -10,13 +10,19 @@ setwd("C:/lab/") # windows
 l2011 <- brick ("p224r63_2011.grd")
 
 # plottiamo la nostra prima immagine usando la funzione plot
-plot()
+plot(l2011)
 
-colorRampPalette () # funzione che serve per creare una scala
-colorRampPalette("black", "grey", light grey")
+# colorRampPalette () # funzione che serve per creare una scala
+# i<- colorRampPalette (c("black", "grey", "light grey")) (100)
 
-c (5, 6, 10, 20, 29) # sono una serie di caratteri e li indico con una c, faccio la stessa cosa con la funzione colorRampPalette
-colorRampPalette(c("black", "grey", light grey")) (100)
+# u <- c (5, 6, 10, 20, 29) # sono una serie di caratteri e li indico con una c, faccio la stessa cosa con la funzione colorRampPalette
+# i <- colorRampPalette(c("black", "grey", "light grey")) (100)
 
 cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
 plot(l2011, col=cl)
+
+# Landsat ETM+
+# b1 = blu
+# b2 = verde
+# b3 = rosso
+# b4 = = infrarosso vicino NIR
