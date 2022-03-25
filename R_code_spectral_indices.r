3c91dd0ef2d9afefb6980d726f30ef575b3d7d11
ciccio-415 <100956742+ciccio-415@users.noreply.github.com>
2022-03-25 17:51:16 +0100

    Create R_code_spectral_indices.r

---
+++ R_code_spectral_indices.r
@@ -0,0 +1,42 @@
+library(raster)
+install.packages("rgdal")
+library(rgdal)
+# abbiamo richiamato la libreria raster che ci serve
+# sttiamo la wd per windows nel mio caso
+setwd("C:/lab/")
+
+import the first file -> defor1_1.jpg -> give it the name l1992
+
+l1992 <- brick("deror_1.jpg")
+
+# plottiamo la foto per capire quali bande ci sono
+plotRGB(l1992, r=1, g=2, b=3, stretch= "lin")
+nel rosso c'è una banda dove riflette molto quindi l'infrarosso vicino (la banda # 1 è la NIR)
+# layer 1= NIR
+# layer 2= red
+# layer 3= green
+
+# facciamo la stessa operazione per la seconda: import the second file -> defor2_.jpg -> give it the name l2006
+l2006 <- brick("deror_2.jpg")
+plotRGB(l2006, r=1, g=2, b=3, stretch= "lin")
+excercise: plot in a multiframe the two images
+
+par(mfrow=c(2,1))
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plotRGB(l2006, r=1, g=2, b=3, stretch="lin")
+
+# DVI differecne vegetation index per i due anni
+dvi1992 = l1992 [[1]] - l1992 [[2]]
+
+# plottiamo il dato del 1992
+dvi1992
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot (dvi1992, col =cl)
+
+dvi2006 = l2006[[1]] - l2006[[2]]
+
+# plottiamo il dato del 2006
+dvi2006
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100) # specifying a color scheme
+plot (dvi2006, col =cl)
+# altro metodo per calcolare il dvi invece di usare gli elemnti potremmo usare i nomi.
