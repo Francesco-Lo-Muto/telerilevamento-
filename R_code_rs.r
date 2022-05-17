337c713a64d949b94d21debd89104204f31a7a7b
ciccio-415 <100956742+ciccio-415@users.noreply.github.com>
2022-05-17 12:29:26 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,15 +1,15 @@
 # questo è il primo script che userò a lezione
 
-# install.packages ("raster")
+# install.packages ("raster") 
 library (raster)
 
-# settaggio cartella di lavoro all'interno del percorso "C"
+# setting working directory within path "C"
 setwd("C:/lab/") # windows
 
-# tramine la funzione brick importiamo la nostra immagine dal paccheto che abbiamo scaricato. la rinominiamo l2011 per facilitare il lavoro successivo
+# through the brick function I can import the image from the downloaded package, then I assign "l2011" name to make subsequent work easier
 l2011 <- brick ("p224r63_2011.grd")
 
-# plottiamo la nostra prima immagine usando la funzione plot
+# I plot my first picture by using plot function
 plot(l2011)
 
 # colorRampPalette () # funzione che serve per creare una scala
