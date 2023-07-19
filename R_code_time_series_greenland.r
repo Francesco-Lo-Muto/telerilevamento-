105e33bbee032c38fb234c59a0a9cda397359f5d
ciccio-415 <100956742+ciccio-415@users.noreply.github.com>
2023-07-19 12:01:19 +0200

    Update R_code_time_series_greenland.r

--- R_code_time_series_greenland.r
+++ R_code_time_series_greenland.r
@@ -1,17 +1,17 @@
-# time series analysis of Greenland LST data
+#Ttime series analysis of Greenland LST data
 
 library (raster)
 
-# setwd("C:/lab/greenland") 
+# Setwd("C:/lab/greenland") 
 
 # I import the data with classic operation and then with a faster method.
 # I want to import a single layer by raster function
 
 lst2000 <- raster("lst_2000.tif")
 lst2000
-# plot lst2000
+# Plot lst2000
 plot(lst2000)
-# exercice: import all the data 
+# Exercice: import all the data 
 lst2005 <- raster("lst_2005.tif")
 lst2010 <- raster("lst_2010.tif")
 lst2015 <- raster("lst_2015.tif")
@@ -23,12 +23,12 @@ plot(lst2000, col=cl)
 plot(lst2005, col=cl)
 plot(lst2010, col=cl)
 plot(lst2015, col=cl)
-# through lapply function I can import everything together
+# Through lapply function I can import everything together
 # I create my list files (in this case is important to work with objects). After that lapply function will apply the raster function to all file
-# list.files (create a files list)
-# import the whole set altogether
+# List.files (create a files list)
+# Import the whole set altogether
 rlist <- list.files (pattern="lst")
-# now I use raster function in this files list
+# Now I use raster function in this files list
 import <- lapply(rlist, raster)
 # I can use stack function to take the layers and put them in a single file
 tgr <- stack (import)
@@ -44,53 +44,53 @@ plotRGB(tgr, r=1, g=2, b=3, stretch="lin")
 plotRGB(tgr, r=4, g=3, b=2, stretch="lin")
 plotRGB(tgr, r=1, g=2, b=4, stretch="lin")
 
-# expamle 2: NO2 decrease during the lockdown period
-# setwd("C:/lab/en") # Windows
-# import first data 
+# Expamle 2: NO2 decrease during the lockdown period
+# Setwd("C:/lab/en") # Windows
+# Import first data 
 en01 <- raster ("EN_0001.png")
-# plot data with a new colourRampPalette
+# Plot data with a new colourRampPalette
 cl <- colorRampPalette(c('red','orange','yellow'))(100)
 plot(en01, col=cl)
-# import thirteenth set
+# Import thirteenth set
 en13 <- raster("EN_0013.png")
 plot(en13, col=cl)
-# let's import the whole set (altogether!)
-# excercise: import the whole as in the greenlan example
-# by the following steps: list.files, lapply, stack
+# Let's import the whole set (altogether!)
+# Excercise: import the whole as in the greenlan example
+# By the following steps: list.files, lapply, stack
 rlist <- list.files(pattern="EN")
 rlist
-# through lapply command I apply raster function to the rlist functionn
+# Through lapply command I apply raster function to the rlist functionn
 list_rast <- lapply(rlist, raster)
 list_rast
-# let's make the stack
+# Let's make the stack
 EN_stack <- stack(list_rast)
 EN_stack
-# now I plot data with a colourRampPalette
+# Now I plot data with a colourRampPalette
 cl <- colorRampPalette(c('red','orange','yellow'))(100)
 plot(EN_stack, col=cl)
-# exercise: plot EN01 besides EN13
+# Exercise: plot EN01 besides EN13
 par(mfrow=c(1,2))
 plot(en[[1]], col=cl)
 plot(en[[13]], col=cl)
 # or 
 en113 <- stack(en[[1]], en[[13]])
 plot(en113, col=cl)
-# let's make the difference:
+# Let's make the difference:
 difen <- en[[1]]- en[[13]]
 cldif <- colorRampPalette(c('blue','white','red'))(100) 
 plot(difen, col=cldif)
-# plotRGB of three files together
+# PlotRGB of three files together
 plotRGB(en, r=1, g=7, b=13, stretch="lin")
 plotRGB(en, r=1, g=7, b=13, stretch="hist")
-# red color is associated with January
-# green color is associated with February
-# blue is associated with March
+# Red color is associated with January
+# Green color is associated with February
+# Blue is associated with March
 
 library(raster)
 library(RStoolbox)
-# setting working directory for winwows
+# Setting working directory for winwows
 setwd("C:/lab/") 
-# import the satellite image
+# Import the satellite image
 so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 so
 # I get a variety of information
